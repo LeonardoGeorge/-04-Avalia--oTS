78de6609ead2533680277c597344dd48bd86d58a
Leonardo George <leonardogeorge30@gmail.com>
2025-02-06 14:09:16 -0300

    Alterações JS & CSS

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-let infeliz = document.querySelector('.emoji-item-0');
-let triste = document.querySelector('.emoji-item-1');
-let neutro = document.querySelector('.emoji-item-2');
-let feliz = document.querySelector('.emoji-item-3');
-let satisfeito = document.querySelector('.emoji-item-4');
+// Selecionando os ícones
+let infeliz = document.querySelector('#emoji-item-0');
+let triste = document.querySelector('#emoji-item-1');
+let neutro = document.querySelector('#emoji-item-2');
+let feliz = document.querySelector('#emoji-item-3');
+let satisfeito = document.querySelector('#emoji-item-4');
 
 // Função para adicionar o destaque de seleção
 function selecionarRating(ratingGroup) {
   // Remover a classe 'checked' de todas as opções
-  infeliz.forEach((item) => item.classList.remove('checked'));
-  triste.forEach((item) => item.classList.remove('checked'));
-  neutro.forEach((item) => item.classList.remove('checked'));
-  feliz.forEach((item) => item.classList.remove('checked'));
-  satisfeito.forEach((item) => item.classList.remove('checked'));
+  [infeliz, triste, neutro, feliz, satisfeito].forEach(item => item.classList.remove('checked'));
 
   // Adicionar a classe 'checked' à opção clicada
-  ratingGroup.forEach((item) => item.classList.add('checked'));
+  ratingGroup.classList.add('checked');
 }
 
-// Adicionar eventos de clique a cada grupo de rating
-infeliz.forEach((item) => {
+// Adicionando eventos de clique a cada ícone
+[infeliz, triste, neutro, feliz, satisfeito].forEach(item => {
   item.addEventListener('click', function () {
-    selecionarRating(infeliz);
+    selecionarRating(item);
   });
 });
 
-triste.forEach((item) => {
-  item.addEventListener('click', function () {
-    selecionarRating(triste);
-  });
-});
 
-neutro.forEach((item) => {
-  item.addEventListener('click', function () {
-    selecionarRating(neutro);
+// Função para efeito de hover
+function adicionarHoverEffect(element) {
+  element.addEventListener('mouseover', function () {
+    element.classList.add('hover');
   });
-});
+  element.addEventListener('mouseout', function () {
+    element.classList.remove('hover');
+  });
+}
 
-feliz.forEach((item) => {
-  item.addEventListener('click', function () {
-    selecionarRating(feliz);
-  });
-});
+// Aplicando o efeito de hover aos ícones
+[infeliz, triste, neutro, feliz, satisfeito].forEach(adicionarHoverEffect);
 
-satisfeito.forEach((item) => {
-  item.addEventListener('click', function () {
-    selecionarRating(satisfeito);
-  });
+// Adicionar eventos de clique para seleção de rating
+infeliz.addEventListener('click', function () {
+  selecionarRating(infeliz);
+});
+triste.addEventListener('click', function () {
+  selecionarRating(triste);
+});
+neutro.addEventListener('click', function () {
+  selecionarRating(neutro);
+});
+feliz.addEventListener('click', function () {
+  selecionarRating(feliz);
+});
+satisfeito.addEventListener('click', function () {
+  selecionarRating(satisfeito);
 });
