a6a8890831f80c68d8a5fa584649a446514e694f
Leonardo George <leonardogeorge30@gmail.com>
2025-02-06 06:57:21 -0300

    alteracao - js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ emojiItems.forEach(item => {
     item.classList.remove('highlight'); // Remove classe de destaque
   });
 });
+
+
+// Seleciona todos os inputs de radio e os labels associados
+const ratings = document.querySelectorAll('input[type="radio"]');
+const labels = document.querySelectorAll('label');
+
+// Adiciona evento de clique para destacar a opção selecionada
+ratings.forEach((rating, index) => {
+    rating.addEventListener('change', () => {
+        // Remove a classe de destaque de todas as opções
+        labels.forEach(label => label.classList.remove('selected'));
+        
+        // Adiciona a classe de destaque à opção selecionada
+        labels[index].classList.add('selected');
+    });
+});
